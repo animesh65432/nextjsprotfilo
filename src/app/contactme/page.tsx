aa2d94308db477a0919713ed4377850d589eb6c1
animesh dutta <kiranduttta234@gmail.com>
2024-05-31 20:21:51 +0530

    fix the errors

--- src/app/contactme/page.tsx
+++ src/app/contactme/page.tsx
@@ -6,8 +6,8 @@ const ContactMe = () => {
       <div className="text-center">
         <h2 className="text-2xl font-bold mb-4">Contact Me</h2>
         <p className="text-lg mb-4">
-          Feel free to get in touch with me via email. I'd love to hear from
-          you!
+          Feel free to get in touch with me via email. I&apos;d love to hear
+          from you!
         </p>
         <a
           href="mailto:kiranduttta234@gmail.com"
